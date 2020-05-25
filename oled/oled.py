a18145af1a39a27f3bc68edd2ecf28250e9414b6
nwgat <github@nwgat.net>
2020-05-25 15:58:36 +0200

    Update oled.py

--- oled/oled.py
+++ oled/oled.py
@@ -1,6 +1,6 @@
-import machine
+import machine ssd1306
 import ssd1306
 i2c = machine.I2C(-1, machine.Pin(5), machine.Pin(4))
 oled = ssd1306.SSD1306_I2C(64, 48, i2c)
 
-#oled.fill(0); oled.text('Hei XD', 6, 2); oled.text('Ninja', 6, 24); oled.show()
+oled.fill(0); oled.text('Go', 6, 2); oled.text('Ninja', 6, 24); oled.text('Go', 6, 30); oled.show()
