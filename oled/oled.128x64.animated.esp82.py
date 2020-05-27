2067c1b6f693b7a26419447f92457153fa1a805d
nwgat <github@nwgat.net>
2020-05-27 02:42:55 +0200

    Create oled.128x64.animated.esp82.py

---
+++ oled/oled.128x64.animated.esp82.py
@@ -0,0 +1,23 @@
+# nwgat.ninja
+
+from machine import I2C, Pin
+import ssd1306, framebuf, time
+
+i2c = I2C(-1, Pin(22), Pin(21))
+display = ssd1306.SSD1306_I2C(128, 64, i2c)
+
+images = []
+for n in range(1,7):
+    with open('seq0000%s.pbm' % n, 'rb') as f:
+        f.readline() # Magic number
+        f.readline() # Dimensions
+        data = bytearray(f.read())
+    fbuf = framebuf.FrameBuffer(data, 128, 64, framebuf.MONO_HLSB)
+    images.append(fbuf)
+
+display.invert(1)
+while True:
+    for i in images:
+        display.blit(i, 0, 0)
+        display.show()
+        time.sleep(0.1)
