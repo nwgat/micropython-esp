25af4214391076ac5b98496de9a19102db8f685c
nwgat <github@nwgat.net>
2020-05-28 01:47:09 +0200

    Create oled.128x64.rssi.8266.py

---
+++ oled/oled.128x64.rssi.8266.py
@@ -0,0 +1,14 @@
+import machine, ssd1306, urequests, network, time
+
+wlan = network.WLAN(network.STA_IF)
+i2c = machine.I2C(-1, machine.Pin(5), machine.Pin(4))
+oled = ssd1306.SSD1306_I2C(64, 48, i2c)
+    
+while True:
+    status = urequests.get("https://raw.githubusercontent.com/nwgat/mailboxninja/master/status")
+    rssi = wlan.status("rssi")
+    time.sleep(1)
+    oled.fill(0)
+    oled.text(status.content, 1, 10)
+    oled.text(str(rssi), 1, 20,) 
+    oled.show()
