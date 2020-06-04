4f46253199dd394ff3327af451a00a7b12ca1609
nwgat <github@nwgat.net>
2020-06-04 13:34:53 +0200

    Update dht22.ESP32.py

--- DHT/dht22.ESP32.py
+++ DHT/dht22.ESP32.py
@@ -1,16 +1,16 @@
 # nwgat.ninja
 # DHT22 / AOSONG AM2302
-# pin 1 > 21(SDA)
-# pin 2 > 3.3v
+# pin 1 > 3.3v
+# pin 2 > 21(SDA)
 # pin 4 > GND
 
-import dht, machine
+import dht, machine, time
 
-d = dht.DHT22(machine.Pin(21))
-dm = d.measure()
-dt = d.temperature()
-dh = d.humidity()
+while True:
 
-print ((dm))
-print ((dt))
-print ((dh))
+ d = dht.DHT22(machine.Pin(21))
+ dm = d.measure()
+ dt = d.temperature()
+ dh = d.humidity()
+ print (dt, dh)
+ time.sleep(120)
